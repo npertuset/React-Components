ef86eceef8933bd53762ecf472bc9ba288278687
Nathan Pertuset <nathanpertuset@gmail.com>
2019-05-05 14:07:28 -0600

    Adding Semantic UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App= () => {
-    return <div>Hi There</div>
+    return (
+        <div className="ui container comments">
+           <CommentDetail />
+           <CommentDetail />
+           <CommentDetail />
+        </div>
+    );
 };
 
 ReactDOM.render(
