b6965b5a0021fe85cf6162dc3d81090a54d7f3e7
Nathan Pertuset <nathanpertuset@gmail.com>
2019-05-05 15:17:47 -0600

    Adding approval card wrapper

--- src/index.js
+++ src/index.js
@@ -2,13 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-const App= () => {
+const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail author="Sam" />
-           <CommentDetail author="Mike" />
-           <CommentDetail author="Jill" />
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure you want to do this? 
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:30pm" 
+                    comment="This is comment"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard> 
+                <CommentDetail 
+                    author="Mike" 
+                    timeAgo="Today at 2:00am" 
+                    comment="This is comment 2"
+                    avatar={faker.image.avatar()}    
+                />
+            </ApprovalCard> 
+            
+            <ApprovalCard> 
+                <CommentDetail 
+                    author="Jill" 
+                    timeAgo="Yesterday at 5:00pm" 
+                    comment="This is comment 3"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard> 
         </div>
     );
 };
